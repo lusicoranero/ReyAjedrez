f0430fc8b94834045b271c9e593d1efb14b424a4
lusicoranero <lusicoranero@gmail.com>
2023-11-21 20:10:48 +0100

    Constructor copia de Posicion

--- src/main/java/org/iesalandalus/programacion/reyajedrez/modelo/Posicion.java
+++ src/main/java/org/iesalandalus/programacion/reyajedrez/modelo/Posicion.java
@@ -5,6 +5,26 @@ public class Posicion {
     private int fila;
     private char columna;
 
+    public Posicion(int fila, char columna) {
+        if (fila < 1) {
+            throw new IllegalArgumentException("El valor de la fila no puede ser inferior a 1.");
+        } else if (fila > 8) {
+            throw new IllegalArgumentException("El valor de la fila no puede ser mayor que 8.");
+        }
+        this.fila = fila;
+        if (columna <'a') {
+            throw new IllegalArgumentException("El valor de la columna no puede ser inferior a a.");
+        } else if (columna >'h') {
+            throw new IllegalArgumentException("El valor de la fila no puede ser mayor que h.");
+        }
+        this.columna = columna;
+    }
+
+    public Posicion(Posicion posicion) {
+        fila = posicion.fila;
+        columna=posicion.columna;
+    }
+
     public int getFila() {
         return fila;
     }
