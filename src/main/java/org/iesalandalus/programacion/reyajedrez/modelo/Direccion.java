27583d30ded1e1bbda8dce1edef93d100fa49eb4
lusicoranero <lusicoranero@gmail.com>
2023-11-21 19:38:33 +0100

    Creación Enum Creación Enum Direccion

---
+++ src/main/java/org/iesalandalus/programacion/reyajedrez/modelo/Direccion.java
@@ -0,0 +1,6 @@
+package org.iesalandalus.programacion.reyajedrez.modelo;
+
+public enum Direccion {
+    NORTE, NORESTE, ESTE, SURESTE, SUR, SUROESTE, OESTE, NOROESTE, ENROQUE_CORTO,ENROQUE_LARGO
+}
+
