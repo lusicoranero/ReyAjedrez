8ff34459d4372819ac66ca06dc97eac0fdbbfff8
lusicoranero <lusicoranero@gmail.com>
2023-11-21 20:19:21 +0100

    Creacion getters y setters de Rey

--- src/main/java/org/iesalandalus/programacion/reyajedrez/modelo/Rey.java
+++ src/main/java/org/iesalandalus/programacion/reyajedrez/modelo/Rey.java
@@ -6,6 +6,27 @@ public class Rey {
     private Color color;
     private int totalMovimientos;
 
-    
+    public Posicion getPosicion() {
+        return posicion;
+    }
 
+    public void setPosicion(Posicion posicion) {
+        this.posicion = posicion;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
+
+    public int getTotalMovimientos() {
+        return totalMovimientos;
+    }
+
+    public void setTotalMovimientos(int totalMovimientos) {
+        this.totalMovimientos = totalMovimientos;
+    }
 }
