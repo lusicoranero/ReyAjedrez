46a7ddefad56e96bfa833bad7cc743f5351ee872
lusicoranero <lusicoranero@gmail.com>
2023-11-21 21:53:46 +0100

    Creacion metodo mover en rey

--- src/main/java/org/iesalandalus/programacion/reyajedrez/modelo/Rey.java
+++ src/main/java/org/iesalandalus/programacion/reyajedrez/modelo/Rey.java
@@ -1,10 +1,12 @@
 package org.iesalandalus.programacion.reyajedrez.modelo;
 
+import javax.naming.OperationNotSupportedException;
+
 public class Rey {
 
     private Posicion posicion;
     private Color color;
-    private int totalMovimientos;
+    private int totalMovimientos=0;
 
     public Rey() {
         color = Color.BLANCO;
@@ -12,7 +14,7 @@ public class Rey {
     }
 
     public Rey(Color color){
-        If(color==Color.BLANCO){
+        if(getColor()==Color.BLANCO){
             posicion.setFila(1);
             posicion.setColumna('e');
         }
@@ -28,7 +30,7 @@ public class Rey {
         return posicion;
     }
 
-    public void setPosicion(Posicion posicion) {
+    private void setPosicion(Posicion posicion) {
         this.posicion = posicion;
     }
 
@@ -36,15 +38,146 @@ public class Rey {
         return color;
     }
 
-    public void setColor(Color color) {
+    private void setColor(Color color) {
         this.color = color;
     }
 
     public int getTotalMovimientos() {
         return totalMovimientos;
     }
+    public void mover (Direccion direccion) throws OperationNotSupportedException {
+        if (direccion==null){
+            throw new NullPointerException("El valor de la dirección no puede ser nulo");
+        }
+        switch (direccion) {
+            case ESTE:
+                if (posicion.getColumna() < 'h') {
+                    posicion.setColumna((char) (posicion.getColumna() + 1));
+                    totalMovimientos=totalMovimientos+1;
+                }
+                else
+                    try {
+                        throw new OperationNotSupportedException("El movimiento no puede ser realizado");
+                    } catch (OperationNotSupportedException e) {
+                        throw new RuntimeException(e);
+                    }
+                break;
+            case SUR:
+                if (posicion.getFila() > 1) {
+                    posicion.setFila(posicion.getFila() - 1);
+                    totalMovimientos=totalMovimientos+1;
+                }
+                else
+                    try {
+                        throw new OperationNotSupportedException("El movimiento no puede ser realizado");
+                    } catch (OperationNotSupportedException e) {
+                        throw new RuntimeException(e);
+                    }
+                break;
+            case NORTE:
+                if (posicion.getFila() < 8) {
+                    posicion.setFila(posicion.getFila() + 1);
+                    totalMovimientos=totalMovimientos+1;
+                }
+                else
+                    try {
+                        throw new OperationNotSupportedException("El movimiento no puede ser realizado");
+                    } catch (OperationNotSupportedException e) {
+                        throw new RuntimeException(e);
+                    }
+                break;
+            case OESTE:
+                if (posicion.getColumna() < 'a') {
+                    posicion.setColumna((char) (posicion.getColumna() - 1));
+                    totalMovimientos = totalMovimientos + 1;
+                }
+                else
+                    try {
+                        throw new OperationNotSupportedException("El movimiento no puede ser realizado");
+                    } catch (OperationNotSupportedException e) {
+                        throw new RuntimeException(e);
+                    }
 
-    public void setTotalMovimientos(int totalMovimientos) {
-        this.totalMovimientos = totalMovimientos;
+                break;
+            case SURESTE:
+                if ((posicion.getColumna() < 'h') && posicion.getFila() > 1) {
+                    posicion.setColumna((char) (posicion.getColumna() + 1));
+                    posicion.setFila(posicion.getFila() - 1);
+                    totalMovimientos=totalMovimientos+1;
+                } else
+                    try {
+                        throw new OperationNotSupportedException("El movimiento no puede ser realizado");
+                    } catch (OperationNotSupportedException e) {
+                        throw new RuntimeException(e);
+                    }
+                break;
+            case NORESTE:
+                if ((posicion.getColumna() < 'h') && posicion.getFila() < 8) {
+                    posicion.setColumna((char) (posicion.getColumna() + 1));
+                    posicion.setFila(posicion.getFila() + 1);
+                    totalMovimientos=totalMovimientos+1;
+                } else
+                    try {
+                        throw new OperationNotSupportedException("El movimiento no puede ser realizado");
+                    } catch (OperationNotSupportedException e) {
+                        throw new RuntimeException(e);
+                    }
+                break;
+            case NOROESTE:
+                if ((posicion.getColumna() < 'a') && posicion.getFila() < 8) {
+                    posicion.setColumna((char) (posicion.getColumna() - 1));
+                    posicion.setFila(posicion.getFila() + 1);
+                    totalMovimientos=totalMovimientos+1;
+                } else
+                    try {
+                        throw new OperationNotSupportedException("El movimiento no puede ser realizado");
+                    } catch (OperationNotSupportedException e) {
+                        throw new RuntimeException(e);
+                    }
+                break;
+            case SUROESTE:
+                if ((posicion.getColumna() < 'a') && posicion.getFila() > 1) {
+                    posicion.setColumna((char) (posicion.getColumna() - 1));
+                    posicion.setFila(posicion.getFila() - 1);
+                    totalMovimientos=totalMovimientos+1;
+                } else
+                    try {
+                        throw new OperationNotSupportedException("El movimiento no puede ser realizado");
+                    } catch (OperationNotSupportedException e) {
+                        throw new RuntimeException(e);
+                    }
+                break;
+
+            case ENROQUE_CORTO:
+                if ((posicion.getColumna() == 'e') && ((posicion.getFila()==1 || posicion.getFila()==8)) && totalMovimientos==0) {
+                    posicion.setColumna((char) (posicion.getColumna() +2));
+                    totalMovimientos=totalMovimientos+1;
+                } else
+                    try {
+                        throw new OperationNotSupportedException("El movimiento no puede ser realizado");
+                    } catch (OperationNotSupportedException e) {
+                        throw new RuntimeException(e);
+                    }
+                break;
+
+            case ENROQUE_LARGO:
+                if ((posicion.getColumna() == 'e') && ((posicion.getFila()==1 || posicion.getFila()==8)) && totalMovimientos==0) {
+                    posicion.setColumna((char) (posicion.getColumna() -2));
+                    totalMovimientos=totalMovimientos+1;
+                } else
+                    try {
+                        throw new OperationNotSupportedException("El movimiento no puede ser realizado");
+                    } catch (OperationNotSupportedException e) {
+                        throw new RuntimeException(e);
+                    }
+                break;
+        }
+
+
+
     }
+
+
+
+
 }
