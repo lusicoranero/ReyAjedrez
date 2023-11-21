d7567de6d4c23f0b7b9c9c1de4dcb3a30fd4670d
lusicoranero <lusicoranero@gmail.com>
2023-11-21 20:04:22 +0100

    Creación de getters y setters de Posicion y excepciones

--- src/main/java/org/iesalandalus/programacion/reyajedrez/modelo/Posicion.java
+++ src/main/java/org/iesalandalus/programacion/reyajedrez/modelo/Posicion.java
@@ -5,4 +5,30 @@ public class Posicion {
     private int fila;
     private char columna;
 
+    public int getFila() {
+        return fila;
+    }
+
+    public void setFila(int fila) {
+        if (fila < 1) {
+            throw new IllegalArgumentException("El valor de la fila no puede ser inferior a 1.");
+        } else if (fila > 8) {
+            throw new IllegalArgumentException("El valor de la fila no puede ser mayor que 8.");
+        }
+        this.fila = fila;
+    }
+
+    public char getColumna() {
+        return columna;
+    }
+
+    public void setColumna(char columna) {
+        if (columna <'a') {
+            throw new IllegalArgumentException("El valor de la columna no puede ser inferior a a.");
+        } else if (columna >'h') {
+            throw new IllegalArgumentException("El valor de la fila no puede ser mayor que h.");
+        }
+        this.columna = columna;
+
+    }
 }
