9f9b991109dd919b916ec8462fde3e80e48cb13f
lusicoranero <lusicoranero@gmail.com>
2023-11-21 20:12:12 +0100

    Equals y Hashcode de Posicion

--- src/main/java/org/iesalandalus/programacion/reyajedrez/modelo/Posicion.java
+++ src/main/java/org/iesalandalus/programacion/reyajedrez/modelo/Posicion.java
@@ -1,5 +1,7 @@
 package org.iesalandalus.programacion.reyajedrez.modelo;
 
+import java.util.Objects;
+
 public class Posicion {
 
     private int fila;
@@ -50,5 +52,18 @@ public class Posicion {
         }
         this.columna = columna;
 
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Posicion posicion = (Posicion) o;
+        return fila == posicion.fila && columna == posicion.columna;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(fila, columna);
     }
 }
