7abf20fdaec51aa1fe4889fade1014c387e6ff9d
lusicoranero <lusicoranero@gmail.com>
2023-11-28 21:00:47 +0100

    Creación atributo de clase Rey

--- src/main/java/org/iesalandalus/programacion/reyajedrez/MainApp.java
+++ src/main/java/org/iesalandalus/programacion/reyajedrez/MainApp.java
@@ -1,7 +1,11 @@
 package org.iesalandalus.programacion.reyajedrez;
 
+import org.iesalandalus.programacion.reyajedrez.modelo.Rey;
+
 public class MainApp {
     public static void main(String[] args) {
+
+        Rey rey=new Rey();
 
     }
 }
