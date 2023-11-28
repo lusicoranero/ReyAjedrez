b776c4bfd9c6b51640e13fdc8821b75b19f7e317
Luis <lusicoranero@gmail.com>
2023-11-28 18:10:06 +0100

    Creación método mostrarMenu()

--- src/main/java/org/iesalandalus/programacion/reyajedrez/Consola.java
+++ src/main/java/org/iesalandalus/programacion/reyajedrez/Consola.java
@@ -1,9 +1,27 @@
 package org.iesalandalus.programacion.reyajedrez;
 
-public class Consola {
+public class Consola{
 
-    private Consola() {
+    private static final Consola consola = new Consola();
+
+    private Consola(){
+
+    }
+
+    private  static void mostrarMenu()
+    {
+        System.out.println("Tarea 3");
+        System.out.println("Elija una opción:");
+        System.out.println("1.- Crear un rey por defecto.");
+        System.out.println("2.- Crear un rey eligiendo el color.");
+        System.out.println("3.- Mover.");
+        System.out.println("0.- Salir");
     }
 
 
+
+
+
+
 }
+
