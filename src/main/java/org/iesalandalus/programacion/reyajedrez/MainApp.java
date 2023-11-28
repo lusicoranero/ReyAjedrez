dc26e654125c8dd06a97310d5c8954b4d01fe480
lusicoranero <lusicoranero@gmail.com>
2023-11-28 21:22:13 +0100

    Creación método mover()

--- src/main/java/org/iesalandalus/programacion/reyajedrez/MainApp.java
+++ src/main/java/org/iesalandalus/programacion/reyajedrez/MainApp.java
@@ -1,5 +1,6 @@
 package org.iesalandalus.programacion.reyajedrez;
 
+import org.iesalandalus.programacion.reyajedrez.modelo.Direccion;
 import org.iesalandalus.programacion.reyajedrez.modelo.Rey;
 
 import static org.iesalandalus.programacion.reyajedrez.Consola.*;
@@ -17,6 +18,13 @@ public class MainApp {
 
     private static void crearReyDefecto(){
         Rey rey = new Rey();
+
+    }
+
+    private static void mover(Direccion direccion){
+        mostrarMenuDirecciones();
+        direccion=elegirDireccion();
+        mover(direccion);
 
     }
 }
